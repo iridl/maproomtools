8c13a291e059c098072aff522f5a6d7b2a7cd636
John del Corral <jdcorral@iri.columbia.edu>
2012-03-05 20:23:21 +0000

    perl script to check the correctness of any RDFa statements in a maproom file. JdC

---
+++ rdfalint.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl -w
+
+# parse a single file for rdfa
+
+if ($#ARGV != 0 ) {
+	print "usage: rdfalint.pl filename or url\neg: rdfalint.pl CMAP_climo.html.en\n";
+	exit;
+}
+  @args = ("xsltproc", "--novalid", "http://iridl.ldeo.columbia.edu/ontologies/xslt/RDFa2RDFXML.xsl", $ARGV[0]);
+  system(@args) == 0
+      or die "system call failed: $?";
+
+
+
+
