6597c545d637bbca0ecf5bc32c90a53f52e9a8cf
John del Corral <jdcorral@iri.columbia.edu>
2012-05-22 12:17:05 -0400

    streamlining of processing and adjustment of namespace declarations.

--- gen_registry.pl
+++ gen_registry.pl
@@ -5,15 +5,19 @@
 # output is the owl file needed for generatentriples
 # there are no arguments to this script
 
+use File::Basename;
+
 if (@ARGV > 0 ) {
-	print "usage: gen_registry.pl with no arguments, in the dir called maproom with map pages below it\n";
+	print "usage: gen_registry.pl with no arguments, in the dir called maproom with map pages below it, like ingrid/maproom\n";
 	exit;
 }
 
-# check for files with rdfa  
-system ('find ./ -exec grep -il rdfa "{}" \; | sort > maproomregistry.prelist');
+# check for files with rdfa content and are not backup files from emacs or being tagged
+# (e.g. ends with .html or .en or .es or .xx, not .en~ or .en.20120506)
 
-# files in this list that begin with <INGRID
+my @exts = qw(.en .es .fr .html .xhtml);
+
+system ('find ./ -exec grep -l XHTML+RDFa "{}" \; | sort > maproomregistry.prelist');
 
 open MP, "<./maproomregistry.prelist" or die "Can't open maproomregistry.prelist: $!\n";
 open OP, ">./maproomregistry.owl" or die "Can't open maproomregistry.owl: $!\n";
@@ -26,7 +30,7 @@ print OP ("  xmlns:rdf=\"http:\/\/www.w3.org\/1999\/02\/22-rdf-syntax-ns#\"\n");
 print OP ("  xmlns:rdfs=\"http:\/\/www.w3.org\/2000\/01\/rdf-schema#\"\n");
 print OP ("  xmlns:owl=\"http:\/\/www.w3.org\/2002\/07\/owl#\"\n");
 print OP ("  xmlns:rdfcache=\"http:\/\/iridl.ldeo.columbia.edu\/ontologies\/rdfcache.owl#\"\n");
-print OP ("  xmlns:newmaproom =\"http:\/\/iri.columbia.edu\/~jdcorral/ingrid\/maproom\/maproomregistry.owl#\">\n");
+print OP ("  xmlns:maproomregistry =\"file:\/\/\/data\/jdcorral\/git_projects\/ingrid\/maproom\/maproomregistry.owl#\">\n");
 print OP ("  <owl:Ontology rdf:about=\"\">\n");
 
 
@@ -34,18 +38,24 @@ while ( $mp = <MP> ) {
   chomp $mp;
 # strip off the leading ./
   $mp =~ s/.\///;
+# find the file extension
+  my ($dir, $name, $ext) = fileparse($mp, @exts);
+
+  if ($ext) {
+
 # open each file and check for string INGRID in 1st line
-  open MP1, "<$mp" or die "Can't open $mp: $!\n";
-  $mp1 = <MP1>;
-  chomp $mp1;
-  if ($mp1 =~ m/INGRID/i) {
-    print ($mp1,"\n");
-    $url = "http:\/\/iridl.ldeo.columbia.edu\/maproom\/".$mp;
-    print OP ("    <newmaproom:importsRdfa rdf:resource=\"",$url,"\"\/>\n");
-} else {
-    print OP ("    <newmaproom:importsRdfa rdf:resource=\"",$mp,"\"\/>\n");
-}
-  close MP1;  
+    open MP1, "<$mp" or die "Can't open $mp: $!\n";
+    $mp1 = <MP1>;
+    chomp $mp1;
+    if ($mp1 =~ m/INGRID/i) {
+      print ($mp1,"\n");
+      $url = "http:\/\/iridl.ldeo.columbia.edu\/maproom\/".$mp;
+      print OP ("    <maproomregistry:importsRdfa rdf:resource=\"",$url,"\"\/>\n");
+    } else {
+      print OP ("    <maproomregistry:importsRdfa rdf:resource=\"",$mp,"\"\/>\n");
+    }
+    close MP1;
+  }  
 }
 # close and remove temporary file
 close MP;
