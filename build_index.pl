d3020ddc5296954289baa4763a9911907214a5ef
John del Corral <jdcorral@iri.columbia.edu>
2013-02-22 10:38:02 -0500

    added sort -unique to sort .gitignore and remove duplicate lines

--- build_index.pl
+++ build_index.pl
@@ -21,8 +21,8 @@ system ("cp $maproomtoolsdir/tab.xslt .");
 
 open IP, "grep xhtml maproomregistry.owl |";
 
-# create .gitignore file in maproom git root dir
-open GI, ">../.gitignore" or die $!;
+# append to .gitignore file in maproom git root dir
+open GI, ">>../.gitignore" or die $!;
 
 while ( $ip = <IP> ) {
   chomp $ip;
@@ -42,22 +42,9 @@ while ( $ip = <IP> ) {
 }
 # close input
 close IP;
-
-# finish writing the other filenames to .gitignore
-print GI "maproom\/canonical_imports.serql\n";
-print GI "maproom\/maproom_section_index.serql\n";
-print GI "maproom\/maproomregistry.owl\n";
-print GI "maproom\/maproomtop.owl\n";
-print GI "maproom\/newmaproomcache\/\n";
-print GI "maproom\/tab.xslt\n";
-print GI "maproom\/tabs.nt\n";
-print GI "maproom\/tabs.xml\n";
-print GI "maproom\/top.nt\n";
-print GI "maproom\/top.xml\n";
-print GI "maproom\/version.xml\n";
-print GI "build.tag\n";
-print GI "*~\n";
-
+# close .gitignore
 close GI;
+# sort .gitignore and remove duplicates
+system ("sort -u ../.gitignore -o ../.gitignore");
 
 
