051bc46ead8819654e365d673b151b15ef3d5fdd
Igor Khomyakov <ikh@ikhsoftware.com>
2013-09-27 14:59:03 -0400

    fixed rdfcachelog.txt issue; cosmetic changes

--- runnewmaproom.pl
+++ runnewmaproom.pl
@@ -1,18 +1,18 @@
 #!/usr/bin/perl
-# local version, all maproom pages have relative paths.
-# this must be run in the directory that has all the maproom subdirs, like /data/jdcorral/iri_html/ingrid/maproom
+# local version, all maproom pages have relative paths. this must be run in the directory that has all the maproom subdirs, like /data/jdcorral/iri_html/ingrid/maproom
 
 use Cwd;
 
-print "Gathering rdfa triples\n";
-
 system("date");
-system("/bin/rm -rf newmaproomcache/*");
-
 my $pwd = cwd();
-print " In $pwd\n";
-system("rdfcache -cache=newmaproomcache file:///$pwd/maproomregistry.owl > newmaproomcache/rdfcachelog.txt");
+print "Setting '$pwd/newmaproomcache' directory\n";
+system("rm -rf newmaproomcache");
+system("mkdir -p newmaproomcache");
 
+print "Gathering rdfa triples\n";
+system("rdfcache -cache=newmaproomcache file:///$pwd/maproomregistry.owl >newmaproomcache/rdfcachelog.txt");
+
+print ("Running CONSTRUCT query for tabs.xml\n");
 #use trig file in newmaproomcache
 open IP, ">maproom_section_index.serql";
 print IP <<EOQ;
@@ -39,13 +39,10 @@ vocab = <http://www.w3.org/1999/xhtml/vocab#>,
 iriterms = <http://iridl.ldeo.columbia.edu/ontologies/iriterms.owl#>,
 maproom = <http://iridl.ldeo.columbia.edu/ontologies/maproom.owl#>
 EOQ
-    close IP;
+close IP;
+system("rdfcache -cache=newmaproomcache -construct=maproom_section_index.serql -constructoutput=./tabs.nt file:///$pwd/maproomregistry.owl >newmaproomcache/rdfcachelog2.txt");
 
-print ("Running CONSTRUCT query for tabs.xml\n");
-
-system("rdfcache -cache=newmaproomcache -construct=maproom_section_index.serql -constructoutput=./tabs.nt file:///$pwd/maproomregistry.owl >/dev/null");
-
-#convert tabs.nt to tabs.xml
+print ("Converting tabs.nt to tabs.xml\n");
 system("rapper -q -i ntriples -o rdfxml-abbrev -f 'xmlns:terms=\"http://iridl.ldeo.columbia.edu/ontologies/iriterms.owl#\"' -f 'xmlns:reg=\"http://iridl.ldeo.columbia.edu/maproom/maproomregistry.owl#\"' -f 'xmlns:map=\"http://iridl.ldeo.columbia.edu/ontologies/maproom.owl#\"' -f 'xmlns:vocab=\"http://www.w3.org/1999/xhtml/vocab#\"' tabs.nt > tabs.xml");
 
 system("date");
