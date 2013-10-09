a599925071cab4689f32fce5cf9bf1b7e7854933
Igor Khomyakov <ikh@ikhsoftware.com>
2013-10-09 14:20:04 -0400

    resolving issue #4

--- build_index.pl
+++ build_index.pl
@@ -26,7 +26,7 @@ while ( $ip = <IP> ) {
 # strip off the leading     <maproomregistry:importsRdfa rdf:resource="
   $ip =~ s/    <maproomregistry:importsRdfa rdf:resource=\"//;
 # strip off the trailing "/>
-  $ip =~ s/\"/>//;
+  $ip =~ s/\"\/>//;
 # create the html version of $ip
   $op = $ip;
   $op =~ s/xhtml/html/;
